898e06a943c622588b3b132ab6f83f301fde45c0
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-03-23 13:06:36 +0200

    Added get all tickets route

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
-router.get('/', (req,res) => {
-    return res.json({message:"success"})
-})
+const Ticket = require("../models/Ticket");
 
+//Get all tickets
+router.get("/", async (req, res) => {
+  let ticketsArray;
+  try {
+    ticketsArray = await Ticket.find({});
+  } catch (error) {
+    return res.status(500).json({ Error: error });
+  }
+  return res.status(200).json(ticketsArray);
+});
 
 module.exports = router;
