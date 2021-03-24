aa74d9841836ffc2987cf84c1d2dea549bc751b0
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-03-24 11:42:25 +0200

    added a post route
    Now can add a new ticket

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 app.use(express.static("client/build"));
 app.use(cors());
+app.use(express.json());
 app.use("/api", api);
 
 module.exports = app;
--- routes/tickets.js
+++ routes/tickets.js
@@ -20,17 +20,18 @@ router.get("/", async (req, res) => {
   try {
     ticketsArray = await Ticket.find({});
   } catch (error) {
-    return res.status(500).json({ Error: error });
+    return res.status(500).json({ Error: error.massage });
   }
   return res.status(200).json(ticketsArray);
 });
 
+//Mark between done and undone
 router.patch("/:ticketId/:isDone", async (req, res) => {
   const { ticketId, isDone } = req.params;
   try {
     await Ticket.findById(ticketId);
   } catch (error) {
-    return res.status(404).json({ error: "Ticket not found" });
+    return res.status(404).json({ Error: "Ticket not found" });
   }
   await Ticket.findByIdAndUpdate(
     ticketId,
@@ -38,11 +39,32 @@ router.patch("/:ticketId/:isDone", async (req, res) => {
     { new: true },
     (err) => {
       if (err) {
-        return res.status(500).json({ Error: err });
+        return res.status(500).json({ Error: err.massage });
       }
     }
   );
   return res.status(200).json({ updated: true });
+});
+
+//Post a new ticket route
+router.post("/", async (req, res) => {
+  const { body } = req;
+  const nowDate = new Date();
+  nowDate.setHours(nowDate.getHours() + 2);
+  const ticket = new Ticket({
+    title: body.title,
+    content: body.content,
+    userEmail: body.userEmail,
+    done: body.done,
+    creationTime: nowDate,
+    labels: body.labels,
+  });
+  try {
+    await ticket.save();
+  } catch (error) {
+    return res.status(500).json({ Error: error.massage });
+  }
+  return res.status(200).json({ Message: "Ticket added" });
 });
 
 module.exports = router;
